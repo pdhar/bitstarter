dc97bb8ce10f6da3296b002e58b277e38a99a182
Ubuntu <ubuntu@ip-10-164-34-113.ec2.internal>
2013-07-10 19:09:04 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,27 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+fs.readFile('index.html', function (err, data) {
+	if (err) throw err;
+	var buffer = new Buffer(data);
+	response.send(buffer.toString('utf-8'));
+    });
+
+
+
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+fs.readFile('index.html', function (err, data) {
+	if (err) throw err;
+	var buffer = new Buffer(data);
+      console.log(buffer.toString('utf-8'));
+    });
+
 });
